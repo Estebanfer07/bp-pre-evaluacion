1a51e681d5443065bffb4dfaffbe4446de367935
Esteban <esteban.fernando07@gmail.com>
2025-09-15 17:14:54 -0500

    fix(dtos): modify client dtos

--- backend/src/main/java/com/esterodr/backend/service/dto/CreateClientWithPersonDto.java
+++ backend/src/main/java/com/esterodr/backend/service/dto/CreateClientWithPersonDto.java
@@ -1,13 +1,19 @@
 package com.esterodr.backend.service.dto;
 
 import com.esterodr.backend.domain.enums.ClientState;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class UpdateClientDto {
+@EqualsAndHashCode(callSuper = true)
+public class CreateClientWithPersonDto extends CreatePersonDto {
+    @NotBlank
     @Size(min = 8, max = 15)
     private String password;
+
+    @NotNull
     private ClientState state;
 }
--- backend/src/main/java/com/esterodr/backend/service/dto/UpdateClientWithPersonDto.java
+++ backend/src/main/java/com/esterodr/backend/service/dto/UpdateClientWithPersonDto.java
@@ -1,22 +1,15 @@
 package com.esterodr.backend.service.dto;
 
-import java.util.UUID;
-
 import com.esterodr.backend.domain.enums.ClientState;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class CreateClientDto {
-    @NotNull
-    private UUID personId;
-
-    @NotBlank
+@EqualsAndHashCode(callSuper = true)
+public class UpdateClientWithPersonDto extends UpdatePersonDto {
     @Size(min = 8, max = 15)
     private String password;
-
-    @NotNull
     private ClientState state;
 }
