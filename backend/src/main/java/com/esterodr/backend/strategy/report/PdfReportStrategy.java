890f0414efa152708979d158d81aa6b2a50f8fdc
Esteban <esteban.fernando07@gmail.com>
2025-09-15 21:16:21 -0500

    feat(report): add initial balance calculation to PDF report generation

--- backend/src/main/java/com/esterodr/backend/strategy/report/PdfReportStrategy.java
+++ backend/src/main/java/com/esterodr/backend/strategy/report/PdfReportStrategy.java
@@ -35,6 +35,19 @@ public class PdfReportStrategy implements ReportStrategy {
         formatted.put("amount", String.format("%.2f", movement.getAmount()));
         formatted.put("balance", String.format("%.2f", movement.getBalance()));
         formatted.put("isCredit", movement.getAmount() > 0);
+        double initialBalance;
+        switch (movement.getMovementType()) {
+            case DEPOSIT:
+                initialBalance = movement.getBalance() - movement.getAmount();
+                break;
+            case WITHDRAWAL:
+            case TRANSFER:
+                initialBalance = movement.getBalance() + movement.getAmount();
+                break;
+            default:
+                initialBalance = movement.getBalance();
+        }
+        formatted.put("initialBalance", String.format("%.2f", initialBalance));
         return formatted;
     }
 }
